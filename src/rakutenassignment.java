a9c3b03cf69f54c693c05f71adcd1eba28f867c1
Sai sruthi <pssruthi123@gmail.com>
2019-04-12 16:57:13 +0800

    Added exceptions and error conditions

--- src/rakutenassignment.java
+++ src/rakutenassignment.java
@@ -6,35 +6,54 @@
  * digits with constants.
  * Step 3: Then the final 10th digit is calculated based on the sum value. If the sum is already divisible 
  * by 11 then 0 is added. If it is 10 then X is added. If not then the value of 11-(sum%11) is added.
+ * Step 4: Exceptions are handled by using try and catch block. Also the error message to enter 12 digits 
+ * is displayed if the productID is not of 12 digits.
  */
 public class rakutenassignment {
 	
 	public static String converttoISBN(Long productID) {
+		
+		try {
+			
 		String id=String.valueOf(productID);
-		
-		// String builder is used instead of string as it is mutable compared to string which is immutable.
-		StringBuilder sb= new StringBuilder();
-		int sum=0;
-		int j=10;
-		
-		//Sum is calculated in the same loop as well as appending it to the output string value.
-		for(int i=3;i<id.length();i++) {
-			String s=id.substring(i,i+1);
-			sum+=Integer.parseInt(s)*j;
-			j--;
-			sb.append(s);
+		if(id.length()==12) {
+			
+			// String builder is used instead of string as it is mutable compared to string which is immutable.
+			StringBuilder sb= new StringBuilder();
+			int sum=0;
+			int j=10;
+			
+			//Sum is calculated in the same loop as well as appending it to the output string value.
+			for(int i=3;i<id.length();i++) {
+				String s=id.substring(i,i+1);
+				sum+=Integer.parseInt(s)*j;
+				j--;
+				sb.append(s);
+			}
+			
+			if (sum%11==0)
+				sb.append(String.valueOf(0));
+			
+			else if (sum%11==1)
+				sb.append("x");
+			
+			else
+				sb.append(String.valueOf(11-(sum%11)));
+			
+			return sb.toString();
+			
 		}
 		
-		if (sum%11==0)
-			sb.append(String.valueOf(0));
+		else {
+			return "Please enter a valid 12 digit";
+		}
 		
-		else if (sum%11==1)
-			sb.append("x");
 		
-		else
-			sb.append(String.valueOf(11-(sum%11)));
+		}
 		
-		return sb.toString();
+		catch (Exception e) {
+			return ("Exception"+ e +" has occured");
+		}
 	}
 	
 	public static void main(String[] args) {
